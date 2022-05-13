5479b1880212db299b5d6822743fa74f5be55008
Awais Amjed <awaisamjed171@gmail.com>
2022-05-13 18:08:23 +0500

    Add relationship between classroom and student

--- student.rb
+++ student.rb
@@ -1,10 +1,13 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
 
     @classroom = classroom
+    classroom.add_student(self)
   end
 
   def play_hooky
