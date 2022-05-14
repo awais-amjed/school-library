35069b7da659c2f7675deee560b806994b5aaf06
Awais Amjed <awaisamjed171@gmail.com>
2022-05-14 08:42:03 +0500

    Add classroom parameter back according to project req

--- student.rb
+++ student.rb
@@ -3,8 +3,10 @@ require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
+
+    @classroom = classroom
   end
 
   def classroom=(classroom)
